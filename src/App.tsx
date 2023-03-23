387ce1a73eb7069784009e4000530b39c9d9d984
Tai Nguyen <nphattai98@gmail.com>
2023-03-23 19:47:55 +0700

    chore: remove empty bucket

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Update again</p>
+        <p>Hello world</p>
         <a
           className="App-link"
           href="https://reactjs.org"
